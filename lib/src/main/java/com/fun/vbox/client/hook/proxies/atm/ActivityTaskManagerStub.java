89cf5d16ea0f3f0809ae485607b47520f366d7ab
anyeway <anyeway@qq.com>
2022-06-06 11:46:35 +0800

    [android 12] atm

--- lib/src/main/java/com/fun/vbox/client/hook/proxies/atm/ActivityTaskManagerStub.java
+++ lib/src/main/java/com/fun/vbox/client/hook/proxies/atm/ActivityTaskManagerStub.java
@@ -1,29 +1,47 @@
 package com.fun.vbox.client.hook.proxies.atm;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.IBinder;
 
+import com.fun.vbox.client.core.VCore;
 import com.fun.vbox.client.hook.annotations.Inject;
 import com.fun.vbox.client.hook.annotations.LogInvocation;
 import com.fun.vbox.client.hook.base.BinderInvocationProxy;
+import com.fun.vbox.client.hook.base.BinderInvocationStub;
 import com.fun.vbox.client.hook.base.MethodProxy;
 import com.fun.vbox.client.hook.base.StaticMethodProxy;
 import com.fun.vbox.client.ipc.VActivityManager;
+import com.fun.vbox.helper.compat.BuildCompat;
+import com.fun.vbox.helper.utils.ComponentUtils;
+import com.fun.vbox.helper.utils.Reflect;
+import com.fun.vbox.os.VUserHandle;
 
 import java.lang.reflect.Method;
 
 import mirror.vbox.app.IActivityTaskManager;
+import mirror.vbox.util.Singleton;
 
 @TargetApi(29)
 @Inject(MethodProxies.class)
 @LogInvocation
 public class ActivityTaskManagerStub extends BinderInvocationProxy {
+//    public ActivityTaskManagerStub() {
+//        super(IActivityTaskManager.Stub.asInterface, "activity_task");
+//    }
+
     public ActivityTaskManagerStub() {
         super(IActivityTaskManager.Stub.asInterface, "activity_task");
+        try {
+            Object object = Reflect.on("android.app.ActivityTaskManager").field("IActivityTaskManagerSingleton").get();
+            Singleton.mInstance.set(object, ((BinderInvocationStub)getInvocationStub()).getProxyInterface());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 
     protected void onBindMethods() {
-        super.onBindMethods();
+        /*super.onBindMethods();
         addMethodProxy(new StaticMethodProxy("activityDestroyed") {
             public Object call(Object obj, Method method, Object... objArr) throws Throwable {
                 VActivityManager.get().onActivityDestroy((IBinder) objArr[0]);
@@ -57,6 +75,52 @@ public class ActivityTaskManagerStub extends BinderInvocationProxy {
                 return MethodProxy.isAppProcess();
             }
         });
-        addMethodProxy(new StaticMethodProxy("requestAutofillData"));
+        addMethodProxy(new StaticMethodProxy("requestAutofillData"));*/
+
+        super.onBindMethods();
+        addMethodProxy(new StaticMethodProxy("activityDestroyed") {
+            public Object afterCall(Object param1Object1, Method param1Method, Object[] param1ArrayOfObject, Object param1Object2) throws Throwable {
+                IBinder iBinder = (IBinder)param1ArrayOfObject[0];
+                VActivityManager.get().onActivityDestroy(iBinder);
+                return super.afterCall(param1Object1, param1Method, param1ArrayOfObject, param1Object2);
+            }
+        });
+        addMethodProxy(new StaticMethodProxy("activityResumed") {
+            public Object call(Object param1Object, Method param1Method, Object... param1VarArgs) throws Throwable {
+                IBinder iBinder = (IBinder)param1VarArgs[0];
+                VActivityManager.get().onActivityResumed(iBinder);
+                return super.call(param1Object, param1Method, param1VarArgs);
+            }
+        });
+        addMethodProxy(new StaticMethodProxy("finishActivity") {
+            public Object call(Object param1Object, Method param1Method, Object... param1VarArgs) throws Throwable {
+                IBinder iBinder = (IBinder)param1VarArgs[0];
+                Intent intent = (Intent)param1VarArgs[2];
+                if (intent != null)
+                    param1VarArgs[2] = ComponentUtils.processOutsideIntent(VUserHandle.myUserId(), VCore.get().isExtPackage(), intent);
+                VActivityManager.get().onFinishActivity(iBinder);
+                return super.call(param1Object, param1Method, param1VarArgs);
+            }
+
+            public boolean isEnable() {
+                return isAppProcess();
+            }
+        });
+        addMethodProxy(new StaticMethodProxy("finishActivityAffinity") {
+            public Object call(Object param1Object, Method param1Method, Object... param1VarArgs) {
+                param1Object = param1VarArgs[0];
+                return Boolean.valueOf(VActivityManager.get().finishActivityAffinity(getAppUserId(), (IBinder)param1Object));
+            }
+
+            public boolean isEnable() {
+                return isAppProcess();
+            }
+        });
+        if (BuildCompat.isSamsung())
+            addMethodProxy(new StaticMethodProxy("startAppLockService") {
+                public Object call(Object param1Object, Method param1Method, Object... param1VarArgs) {
+                    return Integer.valueOf(0);
+                }
+            });
     }
 }
--- lib/src/main/java/com/fun/vbox/client/hook/proxies/atm/MethodProxies.java
+++ lib/src/main/java/com/fun/vbox/client/hook/proxies/atm/MethodProxies.java
@@ -1,70 +1,28 @@
 package com.fun.vbox.client.hook.proxies.atm;
 
-import com.fun.vbox.client.hook.proxies.am.MethodProxies.GetCallingActivity;
-import com.fun.vbox.client.hook.proxies.am.MethodProxies.GetCallingPackage;
-import com.fun.vbox.client.hook.proxies.am.MethodProxies.GetTasks;
-import com.fun.vbox.client.hook.proxies.am.MethodProxies.OverridePendingTransition;
-import com.fun.vbox.client.hook.proxies.am.MethodProxies.SetTaskDescription;
-import com.fun.vbox.client.hook.proxies.am.MethodProxies.StartActivities;
-import com.fun.vbox.client.hook.proxies.am.MethodProxies.StartActivity;
-import com.fun.vbox.client.hook.proxies.am.MethodProxies.StartActivityAndWait;
-import com.fun.vbox.client.hook.proxies.am.MethodProxies.StartActivityAsUser;
-import com.fun.vbox.client.hook.proxies.am.MethodProxies.StartActivityIntentSender;
-import com.fun.vbox.client.hook.proxies.am.MethodProxies.StartActivityWithConfig;
 
 public class MethodProxies {
-    static class GetCallingActivityInAtm extends GetCallingActivity {
-        GetCallingActivityInAtm() {
-        }
-    }
+    static class GetAppTasks extends com.fun.vbox.client.hook.proxies.am.MethodProxies.GetAppTasks {}
 
-    static class GetCallingPackageInAtm extends GetCallingPackage {
-        GetCallingPackageInAtm() {
-        }
-    }
+    static class GetCallingActivityInAtm extends com.fun.vbox.client.hook.proxies.am.MethodProxies.GetCallingActivity {}
 
-    static class GetTasksInAtm extends GetTasks {
-        GetTasksInAtm() {
-        }
-    }
+    static class GetCallingPackageInAtm extends com.fun.vbox.client.hook.proxies.am.MethodProxies.GetCallingPackage {}
 
-    static class OverridePendingTransitionInAtm extends OverridePendingTransition {
-        OverridePendingTransitionInAtm() {
-        }
-    }
+    static class GetTasksInAtm extends com.fun.vbox.client.hook.proxies.am.MethodProxies.GetTasks {}
 
-    static class SetTaskDescriptionInAtm extends SetTaskDescription {
-        SetTaskDescriptionInAtm() {
-        }
-    }
+    static class OverridePendingTransitionInAtm extends com.fun.vbox.client.hook.proxies.am.MethodProxies.OverridePendingTransition {}
 
-    static class StartActivitiesInAtm extends StartActivities {
-        StartActivitiesInAtm() {
-        }
-    }
+    static class SetTaskDescriptionInAtm extends com.fun.vbox.client.hook.proxies.am.MethodProxies.SetTaskDescription {}
 
-    static class StartActivityInAtm extends StartActivity {
-        StartActivityInAtm() {
-        }
-    }
+    static class StartActivitiesInAtm extends com.fun.vbox.client.hook.proxies.am.MethodProxies.StartActivities {}
 
-    static class StartActivityIntentSenderInAtm extends StartActivityIntentSender {
-        StartActivityIntentSenderInAtm() {
-        }
-    }
+    static class StartActivityAndWaitInAtm extends com.fun.vbox.client.hook.proxies.am.MethodProxies.StartActivityAndWait {}
 
-    static class StartActivityAndWaitInAtm extends StartActivityAndWait {
-        StartActivityAndWaitInAtm() {
-        }
-    }
+    static class StartActivityAsUserInAtm extends com.fun.vbox.client.hook.proxies.am.MethodProxies.StartActivityAsUser {}
 
-    static class StartActivityAsUserInAtm extends StartActivityAsUser {
-        StartActivityAsUserInAtm() {
-        }
-    }
+    static class StartActivityInAtm extends com.fun.vbox.client.hook.proxies.am.MethodProxies.StartActivity {}
 
-    static class StartActivityWithConfigInAtm extends StartActivityWithConfig {
-        StartActivityWithConfigInAtm() {
-        }
-    }
+    static class StartActivityIntentSenderInAtm extends com.fun.vbox.client.hook.proxies.am.MethodProxies.StartActivityIntentSender {}
+
+    static class StartActivityWithConfigInAtm extends com.fun.vbox.client.hook.proxies.am.MethodProxies.StartActivityWithConfig {}
 }
