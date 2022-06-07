b4d28efe3169d937d68c914d9cd0bdc55e054345
ayew22 <ayew22@qq.com>
2022-06-07 18:24:33 +0800

    Android 12:  telephony      getCallStateForSubscription hook

--- lib/src/main/java/com/fun/vbox/client/hook/proxies/telephony/TelephonyStub.java
+++ lib/src/main/java/com/fun/vbox/client/hook/proxies/telephony/TelephonyStub.java
@@ -1,29 +1,37 @@
 package com.fun.vbox.client.hook.proxies.telephony;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 
+import com.fun.vbox.client.VClient;
 import com.fun.vbox.client.core.VCore;
 import com.fun.vbox.client.hook.base.BinderInvocationProxy;
+import com.fun.vbox.client.hook.base.MethodProxy;
 import com.fun.vbox.client.hook.base.ReplaceCallingPkgMethodProxy;
 import com.fun.vbox.client.hook.base.ReplaceLastPkgMethodProxy;
+import com.fun.vbox.client.hook.base.ReplaceSpecPkgMethodProxy;
 import com.fun.vbox.client.hook.base.ResultStaticMethodProxy;
 import com.fun.vbox.helper.compat.BuildCompat;
+
+import java.lang.reflect.Method;
 
 import mirror.com.android.internal.telephony.ITelephony;
 
 /**
- *
  * @see android.telephony.TelephonyManager
  */
 public class TelephonyStub extends BinderInvocationProxy {
 
-	public TelephonyStub() {
-		super(ITelephony.Stub.asInterface, Context.TELEPHONY_SERVICE);
-	}
+    public TelephonyStub() {
+        super(ITelephony.Stub.asInterface, Context.TELEPHONY_SERVICE);
+    }
 
-	@Override
-	protected void onBindMethods() {
-		super.onBindMethods();
+    @Override
+    protected void onBindMethods() {
+        super.onBindMethods();
         //phone number
         addMethodProxy(new ReplaceLastPkgMethodProxy("getLine1NumberForDisplay"));
         //fake location
@@ -85,6 +93,61 @@ public class TelephonyStub extends BinderInvocationProxy {
             addMethodProxy(new ResultStaticMethodProxy("setDataEnabled", 0));
             addMethodProxy(new ResultStaticMethodProxy("getDataEnabled", false));
         }
+
+        addMethodProxy(new ReplaceSpecPkgMethodProxy("getCallStateForSubscription", 1));
+        addMethodProxy(new MethodProxy() {
+
+            @Override
+            public boolean beforeCall(Object who, Method method, Object... args) {
+                args[1] = VCore.get().getHostPkg();
+
+                return super.beforeCall(who, method, args);
+            }
+
+            @Override
+            public String getMethodName() {
+                return "getDataNetworkTypeForSubscriber";
+            }
+        });
+
+        addMethodProxy(new MethodProxy() {
+            @Override
+            public boolean beforeCall(Object who, Method method, Object... args) {
+                args[1] = VCore.get().getHostPkg();
+
+                return super.beforeCall(who, method, args);
+            }
+
+            @Override
+            public String getMethodName() {
+                return "getVoiceNetworkTypeForSubscriber";
+            }
+        });
+
+
+        addMethodProxy(new ReplaceCallingPkgMethodProxy("getDeviceIdWithFeature") {
+            @Override
+            public Object call(Object who, Method method, Object... args) throws Throwable {
+                try {
+                    return super.call(who, method, args);
+                } catch (SecurityException e) {
+                    ApplicationInfo ai = VClient.get().getCurrentApplicationInfo();
+                    if (ai.targetSdkVersion >= 29) {
+                        throw e;
+                    }
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        Context context = VCore.get().getContext();
+                        if (context.checkSelfPermission(Manifest.permission.READ_PHONE_STATE)
+                                != PackageManager.PERMISSION_GRANTED) {
+                            // 不排除不检查权限直接使用 try-catch 判断的情况
+                            throw e;
+                        }
+                    }
+                    return null;
+                }
+            }
+        });
+
     }
 
 }
